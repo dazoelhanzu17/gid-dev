d580c4793a2d95c57e24ffc16cfe91a6ab35f198
Dodi <d.setiawann7@gmail.com>
2019-05-14 21:41:52 +0700

    remove usseles module

--- Coreengine/Modules/FileLibrary/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('filelibrary::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('filelibrary.name') !!}
-    </p>
-@stop
